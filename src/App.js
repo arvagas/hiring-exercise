adf9c551242331b38335b8b86745a5d621a7b2b1
arvagas <arvagas@gmail.com>
2021-06-30 20:41:14 -0400

    data sorted by name and list id

--- src/App.js
+++ src/App.js
@@ -10,17 +10,31 @@ function App() {
 
   let url = new URL('https://fetch-hiring.s3.amazonaws.com/hiring.json');
 
-  // Will run once on DOM load; grabs data from supplied url
+  // Will run once on DOM load; grabs data from supplied url and applies filters
   useEffect(() => {
     fetch(url)
       .then(res => res.json())
       .then(res => {
         let tempArr = [];
 
-        // filter out items with blank/null name
+        // Filter out items with blank/null name
         res.forEach(list => {
           if (list.name) tempArr.push(list);
         });
+
+        // Sorts array by name
+        tempArr.sort((a, b) => {
+          // Specifically tailored to naming scheme; will compare number within name
+          let first = parseInt(a.name.slice(5));
+          let second = parseInt(b.name.slice(5));
+
+          if (first > second) return 1;
+          else if (first < second) return -1;
+          else return 0;
+        })
+
+        // Sorts array further by List ID
+        tempArr.sort((a, b) => a.listId - b.listId);
 
         setResults(tempArr);
       });
