36a2055789c504c5b5fa4bb23111af3bbc4c5d93
arvagas <arvagas@gmail.com>
2021-06-30 22:26:22 -0400

    created skeleton structure of table

---
+++ src/components/ItemTable.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ItemTable = ({ listID }) => {
+  return (
+    <table>
+      <tr>
+        <th>List ID {listID}</th>
+      </tr>
+      <tr>
+        <th>ID</th>
+        <th>List ID</th>
+        <th>Name</th>
+      </tr>
+      {
+      }
+    </table>
+  )
+};
+
+export default ItemTable
