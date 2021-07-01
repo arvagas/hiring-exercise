7543ee623e053a048ef6a3f6cdc57cb953d622d4
arvagas <arvagas@gmail.com>
2021-06-30 23:38:30 -0400

    Moved data filter into separate function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // libraries
 import React, { useState, useEffect } from 'react';
 //components
-import ItemTable from './components/ItemTable'
+import ItemTable from './components/ItemTable';
+//utils
+import { dataFilter } from './utils/dataFilter';
 //styles
 import './App.css';
 
@@ -15,44 +17,9 @@ function App() {
     fetch(url)
       .then(res => res.json())
       .then(res => {
-        let tempArr = [];
-        let groupSplit = [];
-        let groupSplitFinal = [];
-        let groupSplitCounter = 1;
+        let modifiedData = dataFilter(res);
 
-        // Filter out items with blank/null name
-        res.forEach(list => {
-          if (list.name) tempArr.push(list);
-        });
-
-        // Sorts array by name
-        tempArr.sort((a, b) => {
-          // Specifically tailored to naming scheme; will compare number within name
-          let first = parseInt(a.name.slice(5));
-          let second = parseInt(b.name.slice(5));
-
-          if (first > second) return 1;
-          else if (first < second) return -1;
-          else return 0;
-        })
-
-        // Sorts array further by List ID
-        tempArr.sort((a, b) => a.listId - b.listId);
-
-        // Separate array into List ID groups
-        tempArr.forEach((list, index) => {
-          if (list.listId === groupSplitCounter) groupSplit.push(list);
-          else {
-            groupSplitFinal.push(groupSplit);
-            groupSplit = [];
-            groupSplitCounter++;
-          }
-
-          // Make sure last item finalizes group split
-          if (index === tempArr.length -1) groupSplitFinal.push(groupSplit);
-        });
-
-        setResults(groupSplitFinal);
+        setResults(modifiedData);
       });
   }, []);
 
